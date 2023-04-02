f9e0030ceede9524e8638c89653da3acd6c5f4c6
Vinícius Viana <viniciusviana020@gmail.com>
2023-04-02 15:15:20 -0300

    Funções aninhadas

---
+++ submission/arquivos_teste/nested/nestedfunctions.asm
@@ -0,0 +1,35 @@
+        >   TEXTO   ; Exportar HEADER
+        >   LOCAL   ;
+        >   GLOBAL  ;
+        >   DEBUG   ;
+        &   /0000
+TEXTO   K   /0024
+LOCAL   K   /000C
+GLOBAL  K   /0000
+DEBUG   K   /0000
+                    ; TEXTO
+        SC  SUM  
+        MM  E
+        PD  /100
+        HM  /0000
+SUM     K   /0000   ; Sub-rotina SUM 
+        SC  MUL     ; AC = (A*C)
+        MM  TEMP    ; TEMP = (A*C)
+        SC  DIV     ; AC = (B/D)
+        AD  TEMP    ; AC = (A*C) + (B/D)
+        RS  SUM
+MUL     K   /0000   ; Sub-rotina MUL 
+        LD  A       
+        ML  C       ; AC = A * C
+        RS  MUL
+DIV     K   /0000   ; Sub-rotina DIV 
+        LD  B       
+        DV  D       ; AC = B / D
+        RS  DIV
+                    ; LOCAL
+A       K   /0053
+B       K   /0450
+C       K   /0100
+D       K   /0010
+E       K   /0000
+TEMP    K   /0000
---
+++ submission/arquivos_teste/nested/nestedfunctions2.asm
@@ -0,0 +1,35 @@
+        >   TEXTO   ; Exportar HEADER
+        >   LOCAL   ;
+        >   GLOBAL  ;
+        >   DEBUG   ;
+        &   /0000
+TEXTO   K   /0024
+LOCAL   K   /000C
+GLOBAL  K   /0000
+DEBUG   K   /0000
+                    ; TEXTO
+        SC  SUM  
+        MM  E
+        PD  /100
+        HM  /0000
+SUM     K   /0000   ; Sub-rotina SUM 
+        SC  SUM1    ; AC = (A + C)
+        MM  TEMP    ; TEMP = (A + C)
+        SC  SUM2    ; AC = (B + D)
+        AD  TEMP    ; AC = (A + C) + (B + D)
+        RS  SUM
+SUM1    K   /0000   ; Sub-rotina SUM1 
+        LD  A       
+        AD  C       ; AC = A + C
+        RS  SUM1
+SUM2    K   /0000   ; Sub-rotina SUM2 
+        LD  B       
+        AD  D       ; AC = B + D
+        RS  SUM2
+                    ; LOCAL
+A       K   /5101
+B       K   /0013
+C       K   /0222
+D       K   /0110
+E       K   /0000
+TEMP    K   /0000
